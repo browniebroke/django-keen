8320893fd17e632539c8420c6b7cb270214dbd73
Jay <ja.geb@me.com>
2014-08-04 17:30:16 +0200

    pip release

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ with open('requirements.txt') as f:
 
 setup(
     name='django-keen',
-    version='0.1.0',
+    version='0.1.2',
     author='Jannis Gebauer',
     author_email='',
     packages=['dkeen',],
